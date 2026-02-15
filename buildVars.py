4f8274ed5eb0a9243f9438ba5ae4ec2bd2b118d1
Noelia Ruiz Martínez <nrm1977@gmail.com>
2026-02-15 14:51:05 +0100

    Update buildVars

--- buildVars.py
+++ buildVars.py
@@ -22,9 +22,10 @@ addon_info = AddonInfo(
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	addon_description=_("""Choose how NVDA should read toast notifications.\nYou can read just the application which sent a toast, and choose if notifications are spoken, displayed in braille or both.\nThis may  improve your experience with toast notifications and help you to keep your privacy."""),
-	# version (unchanged)
+	# version.
 	addon_version="13.0.0",
-	# Brief changelog for this version (unchanged)
+	# Brief changelog for this version.
+	# Translators: Changes for this version of the add-on to be shown in add-on information from add-ons manager.
 	addon_changelog=_("""* Compatible with NVDA 2026.1."""),
 	# Author(s)
 	addon_author="José Fischer <jnunez92@gmail.com>, Noelia Ruiz Martínez <nrm1977@gmail.com>",
